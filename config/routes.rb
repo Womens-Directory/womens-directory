893a3b54cbad07573e67fe230d7a5ceb7c6fda39
Amanda Rodgers <aarodgerswork@gmail.com>
2021-04-18 15:19:02 -0600

    add css to views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'org/show'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'home#home'
   get 'category/:id', to: 'category#show', as: 'show_category'
   get 'location/:id', to: 'location#show', as: 'show_location'
+  get 'org/:id', to: 'org#show', as: 'show_org'
 end
