72e87f5bbcc78aa21e576b36b1cfafc890859dfc
Matt Lewis <matt@mplewis.com>
2021-04-18 15:13:55 -0600

    Disable concurrent asset precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# Fix segmentation fault on assets:precompile - https://github.com/sass/sassc-ruby/issues/207
+Rails.application.config.assets.export_concurrent = false
