470e43f29b884bba2e6b05e047be61a9b2a2a6fd
Amanda Rodgers <aarodgerswork@gmail.com>
2021-04-18 12:29:15 -0600

    add admin frontend

--- Gemfile
+++ Gemfile
@@ -62,3 +62,5 @@ gem "rspec-rails", "~> 5.0"
 gem "annotate", "~> 3.1"
 
 gem "rexml", "~> 3.2"
+
+gem 'rails_admin', '~> 2.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'home#home'
   get 'category/:id', to: 'category#show', as: 'show_category'
 end
