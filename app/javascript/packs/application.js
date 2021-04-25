98d56981476d0e586b721b0cdc66f0a85c8a744b
Amanda Rodgers <aarodgerswork@gmail.com>
2021-04-25 14:28:50 -0600

    adds max width to home page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bulma/bulma.sass"
+import "../style.scss"
 
 Rails.start()
 Turbolinks.start()
