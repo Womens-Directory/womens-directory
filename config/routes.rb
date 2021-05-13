e90a5ff19721c9b72f953709829bc990c4449f39
Matt Lewis <matt@mplewis.com>
2021-05-12 18:40:50 -0600

    feat: authorize with passwordless

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  passwordless_for :users
+
   root to: 'home#home'
   get 'category/:id', to: 'category#show', as: 'show_category'
   get 'location/:id', to: 'location#show', as: 'show_location'
