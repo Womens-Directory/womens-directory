64035185598601dd6b294ea6cd0f824f60b22ed1
Matt Lewis <matt@mplewis.com>
2021-05-15 19:57:35 -0600

    feat: add desc to cats

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,10 +2,11 @@
 #
 # Table name: categories
 #
-#  id         :bigint           not null, primary key
-#  name       :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :bigint           not null, primary key
+#  description :text
+#  name        :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
 #
 class Category < ApplicationRecord
   has_and_belongs_to_many :locations
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_011013) do
+ActiveRecord::Schema.define(version: 2021_05_16_015652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2021_05_13_011013) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
   create_table "categories_locations", id: false, force: :cascade do |t|
