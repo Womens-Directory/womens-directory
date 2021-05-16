c1b09a872910b411566142d8611cc6d09cbcd305
Matt Lewis <matt@mplewis.com>
2021-05-16 14:21:04 -0600

    feat: breadcrumbs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
   include Passwordless::ControllerHelpers
 
-  helper_method :current_user
+  helper_method :current_user, :crumbs
 
   private
 
   def current_user
     @current_user ||= authenticate_by_session(User)
+  end
+
+  def crumbs(category, location)
+    home = helpers.link_to 'Home', '/'
+    cat = helpers.link_to(category.name, show_category_path(category)) if category
+    loc = helpers.link_to(location.name, show_category_location_path(category, location)) if category && location
+    [home, cat, loc].reject(&:blank?).join(' &raquo; ').html_safe
   end
 end
--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -1,5 +1,6 @@
 class LocationController < ApplicationController
   def show
+    @category = Category.find(params[:cat_id]) if params[:cat_id]
     @location = Location.find(params[:id])
   end
 end
