4c96f3d347d9f2c5a5f3fb28f3c55942655792dd
Matt Lewis <matt@mplewis.com>
2021-05-16 14:44:17 -0600

    Navigation breadcrumbs (#34)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 class ApplicationController < ActionController::Base
   include Passwordless::ControllerHelpers
 
-  helper_method :current_user, :top_level_pages
+  helper_method :current_user, :crumbs, :top_level_pages
 
   private
 
   def current_user
     @current_user ||= authenticate_by_session(User)
+  end
+
+  def crumbs(category, location)
+    home = helpers.link_to 'Home', '/'
+    cat = helpers.link_to(category.name, show_category_path(category)) if category
+    loc = helpers.link_to(location.name, show_category_location_path(category, location)) if category && location
+    [home, cat, loc].reject(&:blank?).join(' &raquo; ').html_safe
   end
 
   def root_page
