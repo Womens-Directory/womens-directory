2b70e6f259e9b3391afbb54c726809fc18f35691
Matt Lewis <matt@mplewis.com>
2021-06-07 12:35:13 -0600

    Phone Number links (#45)

--- app/decorators/phone_number_decorator.rb
+++ app/decorators/phone_number_decorator.rb
@@ -2,8 +2,22 @@ class PhoneNumberDecorator < Draper::Decorator
   delegate_all
 
   def pretty
-    d = number.gsub /[^\d]/, ''  # digits
-    return number if d.chars.count != 10
-    "(#{d.slice 0, 3}) #{d.slice 3, 3}-#{d.slice 6, 4}"
+    return @pretty if @pretty
+    d = number.gsub /[^\d]/, ''  # extract digits
+    return @pretty = number if d.chars.count != 10  # don't prettify nums that are not 10 digits long
+    @pretty = "(#{d.slice 0, 3}) #{d.slice 3, 3}-#{d.slice 6, 4}"  # (612) 555-1212
+  end
+
+  def tel_uri
+    @tel ||= "tel:#{pretty}"
+  end
+
+  def sms_uri
+    @sms ||= "sms:#{pretty}"
+  end
+
+  def uri
+    return sms_uri if sms && !call
+    tel_uri
   end
 end
--- spec/decorators/phone_number_decorator_spec.rb
+++ spec/decorators/phone_number_decorator_spec.rb
@@ -1,22 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumberDecorator do
-  subject { pn.decorate }
-  let(:pn) { PhoneNumber.create! number: number }
+  let(:org) { Org.create! name: 'My Org', desc: 'Some desc', website: 'example.com' }
+  let(:loc) { Location.create! org: org, name: 'My Loc', desc: 'Some desc', website: 'example.com',
+                               city: 'Denver', state: 'CO', zip: '80202' }
+  let(:pn) { PhoneNumber.create!(location: loc, number: number, sms: sms, call: call).decorate }
+  let(:sms) { true }
+  let(:call) { true }
 
-  context 'with a number containing ten digits' do
-    let(:number) { '  612 555-1212? ' }
+  describe '#pretty' do
+    subject { pn.pretty }
 
-    it 'formats as an American phone number' do
-      expect(subject.pretty).to eql '(612) 555-1212'
+    context 'with a number containing ten digits' do
+      let(:number) { '  612 555-1212? ' }
+
+      it 'formats as an American phone number' do
+        expect(subject).to eql '(612) 555-1212'
+      end
+    end
+
+    context 'with a phone number that does not have 10 digits' do
+      let(:number) { '+1.651.555.1212' }
+
+      it 'returns the number verbatim' do
+        expect(subject).to eql '+1.651.555.1212'
+      end
     end
   end
 
-  context 'with a phone number that does not have 10 digits' do
-    let(:number) { '+1.651.555.1212' }
+  describe 'URI methods' do
+    let(:number) { '(612) 555-1212' }
 
-    it 'returns the number verbatim' do
-      expect(subject.pretty).to eql '+1.651.555.1212'
+    describe '#sms_uri' do
+      subject { pn.sms_uri }
+      it { is_expected.to eql 'sms:(612) 555-1212' }
+    end
+
+    describe '#tel_uri' do
+      subject { pn.tel_uri }
+      it { is_expected.to eql 'tel:(612) 555-1212' }
+    end
+
+    describe '#uri' do
+      subject { pn.uri }
+
+      context 'with a text-only number' do
+        let(:sms) { true }
+        let(:call) { false }
+        it { is_expected.to eql 'sms:(612) 555-1212' }
+      end
+
+      context 'with a call-only number' do
+        let(:sms) { false }
+        let(:call) { true }
+        it { is_expected.to eql 'tel:(612) 555-1212' }
+      end
+
+      context 'with a number that supports both calls and texts' do
+        let(:sms) { true }
+        let(:call) { true }
+        it { is_expected.to eql 'tel:(612) 555-1212' }
+      end
     end
   end
 end
