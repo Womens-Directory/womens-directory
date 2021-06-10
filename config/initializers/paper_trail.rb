29916350d96d24913214afa596143614bdae60f9
Matt Lewis <matt@mplewis.com>
2021-06-10 15:45:56 -0500

    fix: paper trail init successfully with db

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -1,8 +1,11 @@
-PaperTrail.config.track_associations = true
 PaperTrail::Rails::Engine.eager_load!
+
+PaperTrail.config.track_associations = true
 
 module PaperTrail
   class Version < ::ActiveRecord::Base
-    belongs_to :user, foreign_key: :whodunnit
+    include PaperTrail::VersionConcern
+
+    belongs_to :user, foreign_key: :whodunnit, optional: true
   end
 end
