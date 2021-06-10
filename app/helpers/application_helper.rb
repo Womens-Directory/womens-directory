c0fdaead01f0a03eae42c37713aa17bf3d18261e
Matt Lewis <matt@mplewis.com>
2021-06-09 20:01:48 -0500

    feat: admin_edit_button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def admin_edit_button(inst)
+    return unless current_user
+
+    kind = inst.class.name
+    m = kind.match /(.+)Decorator$/ # trim Decorator from tail end of class name if present
+    kind = m[1] if m
+
+    text = "Edit this #{kind.titlecase.downcase}"
+    attrs = {
+      href: RailsAdmin::Engine.routes.url_helpers.edit_path(kind.underscore.to_sym, inst.id),
+      class: "button is-danger",
+      style: "float: right;",
+    }
+    attrs_str = attrs.map { |k, v| "#{k}=\"#{v}\"" }.join(' ')
+    "<a #{attrs_str}>#{text}</a>".html_safe
+  end
 end
