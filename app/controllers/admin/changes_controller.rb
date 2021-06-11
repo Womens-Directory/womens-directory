d5cd7fec013ae15a1aff27c65c491e8e3a22c76f
Matt Lewis <matt@mplewis.com>
2021-06-10 20:17:51 -0500

    refactor: move logic to decorator

--- app/controllers/admin/changes_controller.rb
+++ app/controllers/admin/changes_controller.rb
@@ -1,21 +1,6 @@
 class Admin::ChangesController < Admin::BaseController
-  IGNORE_CHANGES_TO = %w[created_at updated_at]
   def changes
     @pagy, version_objects = pagy(PaperTrail::Version.order('created_at DESC'))
-    @versions = version_objects.map do |o|
-      changes = {}
-      YAML.load(o.object_changes).each do |k, v|
-        next if IGNORE_CHANGES_TO.include? k
-        from, to = v
-        changes[k] = [from, to]
-      end
-      {
-        obj: o,
-        changes: changes,
-        item: o.item_type.constantize.find(o.item_id),
-        user_email: o.whodunnit ? User.find(o.whodunnit).email : '<unknown>',
-        at: o.created_at,
-      }
-    end
+    @versions = version_objects.map &:decorate
   end
 end
