326d660e83d2a9e32d1a224f95cd7c2daad650d6
Matt Lewis <matt@mplewis.com>
2021-06-15 23:25:16 -0600

    feat: ahoy tracking

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,12 +1,13 @@
 class FeedbackController < ApplicationController
   def save
     p = request.params
-    Feedback.create!(
+    f = Feedback.create!(
       path: p['path'],
       category: p['category'],
       body: p['body'],
       ip: request.remote_ip,
     )
+    ahoy.track :feedback_submitted, feedback_id: f.id
     render plain: '', status: :no_content
   end
 end
