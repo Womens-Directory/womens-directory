b39bfc871b6e1b190a31746577ae993fb378f99e
Matt Lewis <matt@mplewis.com>
2021-06-20 14:21:23 -0600

    fix: dont crash on view locs page

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -2,6 +2,6 @@ class LocationController < ApplicationController
   def show
     @category = Category.find(params[:cat_id]) if params[:cat_id]
     @location = Location.find(params[:id])
-    ahoy.track :location_viewed, category_id: @category.id, location_id: @location.id
+    ahoy.track :location_viewed, category_id: @category&.id, location_id: @location.id
   end
 end
