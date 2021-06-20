1bd9f02640c5453eedcf648dae5df975286e1933
Matt Lewis <matt@mplewis.com>
2021-06-20 14:21:46 -0600

    feat: set title for cat

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,6 +1,7 @@
 class CategoryController < ApplicationController
   def show
     @category = Category.find(params[:id])
+    @title = @category.name
     ahoy.track :category_viewed, category_id: @category.id
   end
 end
