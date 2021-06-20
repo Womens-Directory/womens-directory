188698280defede2a0ba282202b7bbad383c9825
Matt Lewis <matt@mplewis.com>
2021-06-20 14:21:39 -0600

    feat: set title for loc

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -2,6 +2,7 @@ class LocationController < ApplicationController
   def show
     @category = Category.find(params[:cat_id]) if params[:cat_id]
     @location = Location.find(params[:id])
+    @title = [@location, @category].select {|i| i}.map(&:name).join " – "
     ahoy.track :location_viewed, category_id: @category&.id, location_id: @location.id
   end
 end
