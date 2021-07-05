3b83e8e9abea913b6dfd6dd9ee08b2557f9413b7
Matt Lewis <matt@mplewis.com>
2021-07-05 13:10:46 -0600

    feat: anonymize feedback with visits

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -3,11 +3,12 @@ class FeedbackController < ApplicationController
 
   def save
     p = request.params
+    binding.pry
     f = Feedback.create!(
       path: p['path'],
       category: p['category'],
       body: p['body'],
-      ip: request.remote_ip,
+      visit: current_visit,
     )
     notify_new_feedback f
     ahoy.track :feedback_submitted, feedback_id: f.id
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -5,12 +5,13 @@
 #  id         :bigint           not null, primary key
 #  body       :text
 #  category   :string
-#  ip         :string
 #  path       :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  visit_id   :bigint
 #
 class Feedback < ApplicationRecord
   has_many :events, class_name: 'Ahoy::Event'
+  belongs_to :visit, class_name: 'Ahoy::Visit'
   strips_spaces_from_string_fields
 end
