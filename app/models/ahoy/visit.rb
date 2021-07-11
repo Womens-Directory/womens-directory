5fd327ed33d838142b7d6b8efd89f3a4758c8a8e
Matt Lewis <matt@mplewis.com>
2021-07-11 13:11:35 -0600

    fix: stop churn of clear_ip job

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -47,7 +47,7 @@ class Ahoy::Visit < ApplicationRecord
   has_many :events, class_name: "Ahoy::Event"
   belongs_to :user, optional: true
 
-  after_save :clear_ip
+  after_create :clear_ip
 
   def clear_ip
     # Must wait a little bit before clearing the IP so that the visit can be geocoded
