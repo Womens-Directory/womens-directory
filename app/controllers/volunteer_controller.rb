dba508ff38d20f595b8d67bcc8f763717a803a2b
Amanda Rodgers <aarodgerswork@gmail.com>
2021-11-20 19:04:41 -0700

    add submit and send_to_discord methods

--- app/controllers/volunteer_controller.rb
+++ app/controllers/volunteer_controller.rb
@@ -3,5 +3,27 @@ class VolunteerController < ApplicationController
   end
 
   def submit
+    name = params['name']
+    contact = params['contact']
+    hours = params['hours']
+    why = params['why']
+    send_to_discord(name, contact, hours, why)
+  end
+
+  private
+
+  def send_to_discord(name, contact, hours, why)
+    msg = <<~MSG
+      Volunteer info:
+      #{e name}
+      #{e contact}
+      #{e hours} hours per week
+      #{e why}
+    MSG
+    puts msg
+  end
+
+  def e(msg)
+    Markdown.escape msg
   end
 end
