40c426cd4aac7ebe9dcc8157779bde166d9971c1
Amanda Rodgers <aarodgerswork@gmail.com>
2021-11-20 19:22:37 -0700

    add discord webhook

--- app/controllers/volunteer_controller.rb
+++ app/controllers/volunteer_controller.rb
@@ -20,7 +20,9 @@ class VolunteerController < ApplicationController
       #{e hours} hours per week
       #{e why}
     MSG
-    puts msg
+    url = 'https://discord.com/api/webhooks/911800531234734111/IVegDQ5UdFmJ4k0WagwTiSaLOGdQuoGxYv2-hpcsCgPS7I9uzmbF2K2z3z4KDI5B6kdR'
+    body = {content: msg}.to_json
+    RestClient.post url, body, {content_type: :json}
   end
 
   def e(msg)
