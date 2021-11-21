026d94df7a841d87365ecf56f72282d345ec9d3a
Amanda Rodgers <aarodgerswork@gmail.com>
2021-11-20 19:35:26 -0700

    add env variable

--- app/controllers/volunteer_controller.rb
+++ app/controllers/volunteer_controller.rb
@@ -20,9 +20,8 @@ class VolunteerController < ApplicationController
       #{e hours} hours per week
       #{e why}
     MSG
-    url = 'https://discord.com/api/webhooks/911800531234734111/IVegDQ5UdFmJ4k0WagwTiSaLOGdQuoGxYv2-hpcsCgPS7I9uzmbF2K2z3z4KDI5B6kdR'
     body = {content: msg}.to_json
-    RestClient.post url, body, {content_type: :json}
+    RestClient.post ENV['DISCORD_VOLUNTEER_WEBHOOK_URL'], body, {content_type: :json}
   end
 
   def e(msg)
