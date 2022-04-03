a0fb29132a1fec05c1b7b5c7fb53049558122229
Matt Lewis <matt@mplewis.com>
2022-04-03 16:48:47 -0600

    fix: forbid users; allow removal of roles

--- app/logic/user_permissions.rb
+++ app/logic/user_permissions.rb
@@ -3,16 +3,24 @@ module UserPermissions
     def set_all!(current_user, params)
       raise "Not authorized" unless current_user.can? :manage, :user_permissions
 
-      users = {}
+      # Don't let admins lock themselves or other admins out
+      forbidden = User.all.filter { |u| u == current_user || u.superadmin? }
+
+      users_by_id = {}
       matcher = /(\d+)-(.+)/
       params.each do |k, v|
-        result = matcher.match k
-        raise "Invalid key #{k}" unless result
+        next unless result = matcher.match(k) # ignore form keys like `authentication_token`
         id, role = result.captures
-        users[id] ||= User.find(id)
-        puts k, v
+
+        unless users_by_id[id]
+          user = User.find(id)
+          next if forbidden.include?(user)
+          user.roles = []
+          users_by_id[id] = user
+        end
+
         if v == '1'
-          users[id].roles << role.to_sym
+          users_by_id[id].roles << role.to_sym
         elsif v == '0'
           # do nothing
         else
@@ -20,7 +28,9 @@ module UserPermissions
         end
       end
 
-      users.values.each(&:save!)
+      users = users_by_id.values
+      ApplicationRecord.transaction { users.each(&:save!) }
+      return users.count
     end
   end
 end
--- spec/logic/user_permissions_spec.rb
+++ spec/logic/user_permissions_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe UserPermissions do
     end
 
     context 'with a user who is a superadmin' do
-      let(:current_user) { User.new roles: [:superadmin] }
+      let(:current_user) { User.create! id: 0, email: '0', roles: [:superadmin] }
       let!(:user_1) { User.create! id: 1, email: '1' }
       let!(:user_2) { User.create! id: 2, email: '2' }
-      let!(:user_3) { User.create! id: 3, email: '3' }
-      let!(:user_4) { User.create! id: 4, email: '4' }
+      let!(:user_3) { User.create! id: 3, email: '3', roles: [:data_editor] }
+      let!(:user_4) { User.create! id: 4, email: '4', roles: [:superadmin] }
       let(:params) do
         {
           '1-superadmin' => '1',
@@ -46,17 +46,11 @@ RSpec.describe UserPermissions do
 
       it 'changes user permissions as expected' do
         expect { subject }
-          .to change { user_1.reload.roles }.from([]).to(%i[superadmin])
+          .to not_change { current_user.reload.roles }
+          .and change { user_1.reload.roles }.from([]).to(%i[superadmin])
           .and change { user_2.reload.roles }.from([]).to(%i[user_editor cms_editor data_editor])
-          .and change { user_3.reload.roles }.from([]).to(%i[data_contributor])
+          .and change { user_3.reload.roles }.from(%i[data_editor]).to(%i[data_contributor])
           .and not_change { user_4.reload.roles }
-      end
-
-      context 'with invalid key' do
-        let(:params) { { 'asdf-1234' => '1' } }
-        it 'raises the expected error' do
-          expect { subject }.to raise_error "Invalid key asdf-1234"
-        end
       end
 
       context 'with invalid value' do
