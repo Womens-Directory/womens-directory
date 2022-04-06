02673045a149d2c488cca591c8c9e099debbaf54
Matt Lewis <matt@mplewis.com>
2022-04-05 19:38:25 -0600

    feat: authorized users only for invite user

--- app/controllers/admin/user_invites_controller.rb
+++ app/controllers/admin/user_invites_controller.rb
@@ -1,7 +1,14 @@
 class Admin::UserInvitesController < ApplicationController
+  before_action :require_authorized_user!
+
   def new
   end
 
   def create
+  end
+
+  def require_authorized_user!
+    require_user!
+    authorize! :invite, User
   end
 end
