742c75290af2157996bf909ca86b1ee7a215dcd4
Matt Lewis <matt@mplewis.com>
2022-04-05 19:20:50 -0600

    fix: dont let user inviters manage all users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -33,10 +33,9 @@ class Ability
       can :manage, :all
     end
 
-    # User editors can invite and edit users.
+    # User inviters can invite new contributors.
     if user.user_inviter?
-      # TODO: Users should not be able to edit other user editors
-      can :manage, User
+      can :invite, User
     end
 
     # CMS editors can manage all pages and content within the CMS.
