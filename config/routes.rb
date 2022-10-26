bd14921dc063f101b771b464befc67312e095e81
Matt Lewis <matt@mplewis.com>
2022-10-25 21:08:31 -0600

    feat: user form

--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,9 @@ Rails.application.routes.draw do
   get 'search', to: 'search#search'
   post 'feedback', to: 'feedback#save', as: 'save_feedback'
 
+  get 'submission', to: 'user_submissions#form', as: 'submission_form'
+  post 'submission', to: 'user_submissions#create', as: 'create_submission'
+  get 'submission/confirm/:token', to: 'user_submissions#confirm', as: 'confirm_submission'
+
   comfy_route_cms path: '/'
 end
