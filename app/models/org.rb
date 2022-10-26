9ed004641fc9af05d3b7355f26280aa25607af0f
Matt Lewis <matt@mplewis.com>
2022-10-25 20:18:52 -0600

    feat: submission model <-> loc, org models; org visibility

--- app/models/org.rb
+++ app/models/org.rb
@@ -2,16 +2,27 @@
 #
 # Table name: orgs
 #
-#  id         :bigint           not null, primary key
-#  desc       :text             not null
-#  name       :string           not null
-#  website    :string           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id            :bigint           not null, primary key
+#  desc          :text             not null
+#  name          :string           not null
+#  visible       :boolean          default(FALSE), not null
+#  website       :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  submission_id :bigint
+#
+# Indexes
+#
+#  index_orgs_on_submission_id  (submission_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (submission_id => submissions.id)
 #
 class Org < ApplicationRecord
   include PgSearch::Model
 
+  belongs_to :submission, optional: true
   has_many :locations, dependent: :destroy
   has_many :events, class_name: 'Ahoy::Event', dependent: :destroy
   validates_presence_of :desc, :name, :website
