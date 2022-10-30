0e272aea57ffe018d546e89bfbec6440a231feec
Matt Lewis <matt@mplewis.com>
2022-10-30 15:18:45 -0700

    fix: dont init flipper if in rakefile

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -1,11 +1,15 @@
-# Ensure all features are registered in the UI
-Features::FEATURES.each do |feature, _|
-  Flipper.add feature
+def setup_flipper
+  # Ensure all features are registered in the UI
+  Features::FEATURES.each do |feature, _|
+    Flipper.add feature
+  end
+
+  Flipper::UI.configure do |config|
+    config.descriptions_source = ->(keys) do
+      Features::FEATURES.transform_keys(&:to_s)
+    end
+    config.show_feature_description_in_list = true
+  end
 end
 
-Flipper::UI.configure do |config|
-  config.descriptions_source = ->(keys) do
-    Features::FEATURES.transform_keys(&:to_s)
-  end
-  config.show_feature_description_in_list = true
-end
+setup_flipper unless ENV['RAKE']
