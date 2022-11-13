07365f7553228bab21cc2d6eb5761f1de93a4798
Matt Lewis <matt@mplewis.com>
2022-11-13 13:11:28 -0700

    feat: factories; refactor create location spec

--- spec/system/fresh/create_location_spec.rb
+++ spec/system/fresh/create_location_spec.rb
@@ -1,26 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'User Submission', type: :system do
-  before do
-    driven_by :rack_test
+  before { driven_by :rack_test }
 
-    Category.create! id: 1, name: 'Diapers'
-    Category.create! id: 2, name: 'Energy Assistance'
-    Category.create! id: 3, name: 'Food Banks'
-
-    Org.create! visible: true, name: 'ACME Assistance', desc: '...', website: '...'
-  end
-
-  let(:admin_user) { User.create! email: 'contributor@womensdirectory.org', roles: %i[superadmin] }
+  let!(:cat_diapers) { create :category, name: 'Diapers' }
+  let!(:cat_energy) { create :category, name: 'Energy Assistance' }
+  let!(:cat_food) { create :category, name: 'Food Banks' }
+  let!(:org) { create :org, name: 'ACME Assistance' }
 
   it 'works' do
-    passwordless_sign_in admin_user
+    passwordless_sign_in create(:superadmin)
     visit admin_fresh_new_location_path
     expect(page).to have_content 'Create New Location'
 
     select 'ACME Assistance', from: 'Organization'
-    fill_in 'location[name]', with: 'ACME Womens Assistance - Denver'
-    fill_in 'location[desc]', with: 'Our Denver location provides...'
+    fill_in 'location[name]', with: 'ACME Assistance - Los Angeles'
+    fill_in 'location[desc]', with: 'Our Los Angeles location provides...'
     fill_in 'location[city]', with: 'Los Angeles'
     select 'California', from: 'State'
     fill_in 'location[zip]', with: '90012'
@@ -31,6 +26,6 @@ RSpec.describe 'User Submission', type: :system do
     expect { click_on 'Create Location' }.to change { Location.count }.by(1)
     expect(page).to have_content 'Location created successfully.'
     expect(Location.last).to be_visible
-    expect(Location.last.categories).to contain_exactly Category.find(1), Category.find(3)
+    expect(Location.last.categories).to contain_exactly cat_diapers, cat_food
   end
 end
