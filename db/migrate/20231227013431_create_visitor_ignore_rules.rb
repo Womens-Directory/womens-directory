2074d0bfcef5ab68f426197cb6d5c607ff397666
Matt Lewis <matt@mplewis.com>
2023-12-26 19:16:47 -0700

    fix: cascade on delete rule

--- db/migrate/20231227013431_create_visitor_ignore_rules.rb
+++ db/migrate/20231227013431_create_visitor_ignore_rules.rb
@@ -7,7 +7,7 @@ class CreateVisitorIgnoreRules < ActiveRecord::Migration[6.1]
 
     create_table :visitor_ignores do |t|
       t.string :visitor_token, unique: true, null: false
-      t.references :visitor_ignore_rule, foreign_key: true, null: false
+      t.references :visitor_ignore_rule, foreign_key: true, null: false, on_delete: :cascade
     end
   end
 end
