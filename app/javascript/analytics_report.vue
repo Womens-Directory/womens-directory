57989a60f69f6df1d0d128c71f6ee16b022a1499
Matt Lewis <matt@mplewis.com>
2023-12-28 15:26:20 -0700

    feat: tabs & show aggregate methods

--- app/javascript/analytics_report.vue
+++ app/javascript/analytics_report.vue
@@ -1,16 +1,64 @@
 <template>
 	<div>
-		<div>
-			<h2>Locations Viewed</h2>
-			<canvas id="graph-loc"></canvas>
+		<div class="tabs is-boxed">
+			<ul>
+				<li :class="{ 'is-active': currentTab === 'total-views' }"><a @click="selectTab('total-views')">Total Views</a>
+				</li>
+				<li :class="{ 'is-active': currentTab === 'by-visit' }"><a @click="selectTab('by-visit')">By Visit</a></li>
+				<li :class="{ 'is-active': currentTab === 'by-visitor' }"><a @click="selectTab('by-visitor')">By Visitor</a></li>
+			</ul>
 		</div>
-		<div>
-			<h2>Categories of Locations Viewed</h2>
-			<canvas id="graph-cat-loc"></canvas>
+
+		<div data-graph-aggregate="total-views" :class="{ 'is-hidden': currentTab !== 'total-views' }">
+			<h1>Total Views</h1>
+			<p>Counting all page views, even multiple views of the same page by the same visitor during the same visit.</p>
+			<div>
+				<h2>Locations Viewed</h2>
+				<canvas data-graph-content="loc"></canvas>
+			</div>
+			<div>
+				<h2>Categories of Locations Viewed</h2>
+				<canvas data-graph-content="cat-loc"></canvas>
+			</div>
+			<div>
+				<h2>Categories Viewed</h2>
+				<canvas data-graph-content="cat"></canvas>
+			</div>
 		</div>
-		<div>
-			<h2>Categories Viewed</h2>
-			<canvas id="graph-cat"></canvas>
+
+		<div data-graph-aggregate="by-visit" :class="{ 'is-hidden': currentTab !== 'by-visit' }">
+			<h1>By Visit</h1>
+			<p>Pages viewed multiple times during a single visit are counted once. Views on separate visits are counted multiple
+				times.</p>
+			<div>
+				<h2>Locations Viewed</h2>
+				<canvas data-graph-content="loc"></canvas>
+			</div>
+			<div>
+				<h2>Categories of Locations Viewed</h2>
+				<canvas data-graph-content="cat-loc"></canvas>
+			</div>
+			<div>
+				<h2>Categories Viewed</h2>
+				<canvas data-graph-content="cat"></canvas>
+			</div>
+		</div>
+
+		<div data-graph-aggregate="by-visitor" :class="{ 'is-hidden': currentTab !== 'by-visitor' }">
+			<h1>By Visitor</h1>
+			<p>Pages viewed multiple times by the same visitor are only counted once, even if they are on separate visits.</p>
+			<div>
+				<h2>Locations Viewed</h2>
+				<canvas data-graph-content="loc"></canvas>
+			</div>
+			<div>
+				<h2>Categories of Locations Viewed</h2>
+				<canvas data-graph-content="cat-loc"></canvas>
+			</div>
+			<div>
+				<h2>Categories Viewed</h2>
+				<canvas data-graph-content="cat"></canvas>
+			</div>
 		</div>
 	</div>
 </template>
@@ -28,7 +76,7 @@ import {
 	BarController
 } from 'chart.js'
 import ColorHash from 'color-hash'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 
 const colorHash = new ColorHash({ saturation: 0.7 })
 
@@ -124,9 +172,15 @@ function render(elem: HTMLCanvasElement, data: Graphable[], maxCount?: number) {
 }
 
 const dataSets: Record<string, { data: Graphable[], maxCount?: number }> = {
-	'#graph-loc': { data: Object.values(viewsByID.location), maxCount: 20 },
-	'#graph-cat-loc': { data: Object.values(viewsByID.cat_loc) },
-	'#graph-cat': { data: Object.values(viewsByID.cat) },
+	'div[data-graph-aggregate="total-views"] canvas[data-graph-content="loc"]': { data: Object.values(viewsByID.location), maxCount: 20 },
+	'div[data-graph-aggregate="total-views"] canvas[data-graph-content="cat-loc"]': { data: Object.values(viewsByID.cat_loc) },
+	'div[data-graph-aggregate="total-views"] canvas[data-graph-content="cat"]': { data: Object.values(viewsByID.cat) },
+	'div[data-graph-aggregate="by-visit"] canvas[data-graph-content="loc"]': { data: Object.values(props.data.views_by_visit.location), maxCount: 20 },
+	'div[data-graph-aggregate="by-visit"] canvas[data-graph-content="cat-loc"]': { data: Object.values(props.data.views_by_visit.cat_loc) },
+	'div[data-graph-aggregate="by-visit"] canvas[data-graph-content="cat"]': { data: Object.values(props.data.views_by_visit.cat) },
+	'div[data-graph-aggregate="by-visitor"] canvas[data-graph-content="loc"]': { data: Object.values(props.data.views_by_visitor.location), maxCount: 20 },
+	'div[data-graph-aggregate="by-visitor"] canvas[data-graph-content="cat-loc"]': { data: Object.values(props.data.views_by_visitor.cat_loc) },
+	'div[data-graph-aggregate="by-visitor"] canvas[data-graph-content="cat"]': { data: Object.values(props.data.views_by_visitor.cat) },
 }
 
 onMounted(() => {
@@ -136,4 +190,10 @@ onMounted(() => {
 		})
 	}, 0)
 })
+
+const currentTab = ref('total-views')
+
+function selectTab(name: string) {
+	currentTab.value = name
+}
 </script>
