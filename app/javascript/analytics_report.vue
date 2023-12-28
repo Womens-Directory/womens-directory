be3f744ede262818865250c8ffb48c0d07c209e9
Matt Lewis <matt@mplewis.com>
2023-12-27 17:03:32 -0700

    feat: pie graphs

--- app/javascript/analytics_report.vue
+++ app/javascript/analytics_report.vue
@@ -1,9 +1,25 @@
 <template>
-	<h1>Hello world!</h1>
+	<div>
+		<div class="pie">
+			<h2>Categories of Locations Viewed</h2>
+			<Pie :data="catLocData" />
+		</div>
+		<div class="pie">
+			<h2>Categories Viewed</h2>
+			<Pie :data="catData" />
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { defineComponent } from 'vue'
+import { Pie } from 'vue-chartjs'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
 type LocationSummary = {
+	count: number,
 	id: number,
 	name: string,
 	desc: string,
@@ -12,6 +28,7 @@ type LocationSummary = {
 }
 
 type CategorySummary = {
+	count: number,
 	id: number,
 	name: string,
 	desc: string,
@@ -19,6 +36,7 @@ type CategorySummary = {
 }
 
 type OrgSummary = {
+	count: number,
 	id: number,
 	name: string,
 	desc: string,
@@ -27,31 +45,64 @@ type OrgSummary = {
 }
 
 type CmsPageSummary = {
+	count: number,
 	id: number,
 	name: string,
 	link: string,
 }
 
 type Data = {
-	data: {
-		visit_count: number,
-		event_count: number,
-		event_types: { [key: string]: number },
-		views_by_id: {
-			location: { [key: string]: LocationSummary },
-			cat_loc: { [key: string]: CategorySummary },
-			cat: { [key: string]: CategorySummary },
-			org: { [key: string]: OrgSummary },
-			cms_page: { [key: string]: CmsPageSummary },
-		},
-	}
+	visit_count: number,
+	event_count: number,
+	event_types: { [key: string]: number },
+	views_by_id: {
+		location: { [key: string]: LocationSummary },
+		cat_loc: { [key: string]: CategorySummary },
+		cat: { [key: string]: CategorySummary },
+		org: { [key: string]: OrgSummary },
+		cms_page: { [key: string]: CmsPageSummary },
+	},
 }
 
-const props = defineProps<Data>()
+const props = defineProps<{ data: Data }>()
+
+// const categoryData = {
+// 	labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+// 	datasets: [
+// 		{
+// 			backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+// 			data: [40, 20, 80, 10]
+// 		}
+// 	]
+// }
+
+const viewsByID = props.data.views_by_id
+
+const catLocData = {
+	labels: [], datasets: [{ data: [] }]
+}
+Object.values(viewsByID.cat_loc).sort((a, b) => b.count - a.count).forEach((cat) => {
+	catLocData.labels.push(cat.name)
+	catLocData.datasets[0].data.push(cat.count)
+})
+
+const catData = {
+	labels: [], datasets: [{ data: [] }]
+}
+Object.values(viewsByID.cat).sort((a, b) => b.count - a.count).forEach((cat) => {
+	catData.labels.push(cat.name)
+	catData.datasets[0].data.push(cat.count)
+})
+
+
 </script>
 
 <style scoped>
 h1 {
 	color: red;
+}
+
+.pie {
+	max-width: 400px;
 }
 </style>
